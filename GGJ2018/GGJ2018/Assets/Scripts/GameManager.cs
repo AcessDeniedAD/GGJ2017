0393813bbea94f802875ac91f2458b93d69914ef
MaximeGammaitoni <m.gammaitoni@student.isartdigital.com>
2018-01-25 23:35:07 +0100

    add deux trois trucs

---
+++ GGJ2018/GGJ2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    public static GameManager singleton;
+
+    public delegate void EVENT_GAME_MANAGER();
+    public static event EVENT_GAME_MANAGER OnLevelFail;
+    public static event EVENT_GAME_MANAGER OnLevelWin;
+
+    void Start () {
+        if (singleton == null)
+        {
+            singleton = this;
+        }
+        else if (singleton != this)
+        {
+            DestroyManager();
+            return;
+        }
+    }
+
+    public void DestroyManager()
+    {
+        Destroy(gameObject);
+        StopAllCoroutines();
+        return;
+    }
+
+    public void LevelFail()
+    {
+        OnLevelFail();
+    }
+}
---
+++ GGJ2018/GGJ2018/Assets/Scripts/test.cs
@@ -0,0 +1,29 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    // Use this for initialization
+    void Start()
+    {
+        initialPosition = transform.position;
+        GameManager.OnLevelFail += () => {
+            ReplaceCube();
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GameManager.singleton.LevelFail();
+        }
+        
+    }
+
+    public void ReplaceCube()
+    {
+        transform.position = initialPosition;
+    }
+}*/
