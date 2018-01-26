f49e97afa6c387325998cbb1c28aae27551f50cc
MaximeGammaitoni <m.gammaitoni@student.isartdigital.com>
2018-01-26 21:30:31 +0100

    fhjgjfghf

---
+++ GGJ2018/GGJ2018/Assets/Scripts/Input2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+
+public class Input2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GamePad.GetButtonDown(GamePad.Button.A, GamePad.Index.One)) {
+            Debug.Log("okokokokoko");
+        }
+	}
+}
