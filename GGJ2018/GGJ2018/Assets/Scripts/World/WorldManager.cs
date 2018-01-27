a134ae478f5da0abc1e241a47a0b5cba8059f29c
MaximeGammaitoni <m.gammaitoni@student.isartdigital.com>
2018-01-27 02:02:15 +0100

    push bug de jouhn + quelques truc

---
+++ GGJ2018/GGJ2018/Assets/Scripts/World/WorldManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour {
+
+    public int speed;
+    public List<Transform> floors;
+
+
+	// Use this for initialization
+	void Start () {
+        int childNumber = transform.childCount;
+        for (int i = 0; i < childNumber; i++) {
+            floors.Add(transform.GetChild(i));
+        }
+	}
+
+    private void Update()
+    {
+        for (int i = 0; i < floors.Count; i++)
+        {
+            floors[i].transform.position += -Vector3.forward * speed * Time.deltaTime;
+
+            if (floors[i].transform.position.z < -35 ){
+
+                Destroy(floors[i].gameObject);
+                floors.Remove(floors[i]);
+            }
+        }
+    }
+}
