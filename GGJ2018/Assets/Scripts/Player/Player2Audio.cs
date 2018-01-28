598846b68a1f34e81c41a51c47e812cbf2f9c7a5
John Touba <tojo@real.lu>
2018-01-28 14:51:06 +0100

    m

---
+++ GGJ2018/Assets/Scripts/Player/Player2Audio.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Audio : MonoBehaviour {
+
+	AudioSource audioSource;
+	public  List<AudioClip>  listAudios;
+
+	void Start()
+	{
+		StartCoroutine (musiiiiiiic ());
+	}
+
+	void Update()
+	{
+
+		
+	}
+	IEnumerator musiiiiiiic(){
+		while (true) {
+			audioSource = GetComponent<AudioSource> ();
+			int audioRand = Random.Range (0, listAudios.Count);
+			Debug.Log ("RAND");
+			Debug.Log (listAudios.Count);
+			Debug.Log (audioRand);
+			AudioClip audio = listAudios [audioRand];
+			int time = Random.Range (4, 40);
+			//Play the audio you attach to the AudioSource component
+			audioSource.PlayOneShot (audio);
+
+			yield return new WaitForSeconds ((float)time);
+		}
+		}
+}
